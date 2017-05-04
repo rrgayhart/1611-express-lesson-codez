88b0a02fb36d82069e06c858b210e038800d90fc
rrgayhart <rrgayhart@gmail.com>
2017-05-04 11:11:48 -0600

    fix linting errors

--- test/server-test.js
+++ test/server-test.js
@@ -5,35 +5,35 @@ var request = require('request');
 var app = require('../server');
 
 describe('Server', function(){
-  before(function(done){
-      this.port = 9876;
-      this.server = app.listen(this.port, function(err, result){
-        if (err) { return done(err); }
-        done();
-      });
+	before(function(done){
+		this.port = 9876;
+		this.server = app.listen(this.port, function(err){
+			if (err) { return done(err); }
+			done();
+		});
 
-      this.request = request.defaults({
-        baseUrl: 'http://localhost:9876/'
-      });
-  });
+		this.request = request.defaults({
+			baseUrl: 'http://localhost:9876/'
+		});
+	});
 
-  after(function(){
-    this.server.close();
-  });
+	after(function(){
+		this.server.close();
+	});
 
-  it('should exist', function(){
-    assert(app);
-  });
+	it('should exist', function(){
+		assert(app);
+	});
 
-  describe('GET /', function(){
-    it('should return a 200', function(done){
-      this.request.get('/', function(error, response){
-        if (error) { return done(error) }
-        assert.equal(response.statusCode, 200);
-        done();
-      });
-    });
-  });
+	describe('GET /', function(){
+		it('should return a 200', function(done){
+			this.request.get('/', function(error, response){
+				if (error) { return done(error); }
+				assert.equal(response.statusCode, 200);
+				done();
+			});
+		});
+	});
 });
 
 
