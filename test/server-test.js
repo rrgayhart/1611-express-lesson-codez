4accac8332d87f78179093f469e9b749cf365870
rrgayhart <rrgayhart@gmail.com>
2017-05-04 11:09:50 -0600

    First juicy test

--- test/server-test.js
+++ test/server-test.js
@@ -1,23 +1,47 @@
 // test/server-test.js
 
 var assert = require('chai').assert;
+var request = require('request');
 var app = require('../server');
 
 describe('Server', function(){
-	before(function(done){
-		this.port = 9876;
-		this.server = app.listen(this.port, function(err, result){
-			if (err) { return done(err); }
-			done();
-		});
-	}
-  );
+  before(function(done){
+      this.port = 9876;
+      this.server = app.listen(this.port, function(err, result){
+        if (err) { return done(err); }
+        done();
+      });
 
-	after(function(){
-		this.server.close();
-	});
+      this.request = request.defaults({
+        baseUrl: 'http://localhost:9876/'
+      });
+  });
 
-	it('should exist', function(){
-		assert(app);
-	});
+  after(function(){
+    this.server.close();
+  });
+
+  it('should exist', function(){
+    assert(app);
+  });
+
+  describe('GET /', function(){
+    it('should return a 200', function(done){
+      this.request.get('/', function(error, response){
+        if (error) { return done(error) }
+        assert.equal(response.statusCode, 200);
+        done();
+      });
+    });
+  });
 });
+
+
+
+
+
+
+
+
+
+
